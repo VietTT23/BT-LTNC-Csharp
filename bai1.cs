6f437b75f4ad2532880318cde62c041bdc4fc47a
VietTT23 <62922146+VietTT23@users.noreply.github.com>
2020-06-21 22:27:33 +0700

    bai tap ve cau truc dieu khien

---
+++ bai1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hw_1
+{
+    class bai1
+    {
+        static void Main(string[] args)
+        {
+            int Day, Month, Year;
+            Console.WriteLine("Nhap ngay, thang, nam:");
+            Console.Write("Day : ");
+            Day = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Month : ");
+            Month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Year : ");
+            Year = Convert.ToInt32(Console.ReadLine());
+
+            if (Month == 1 || Month == 3 || Month == 5 || Month == 7 || Month == 8 || Month == 10 || Month == 12)
+            {
+                if (Day >= 1 && Day <= 31)
+                    Console.WriteLine("{0}/{1}/{2} la ngay hop le", Day, Month, Year);
+                else
+                    Console.WriteLine("{0} ngay khong hop le", Day);
+            }
+
+            if (Month == 9 || Month == 6 || Month == 4 || Month == 11)
+            {
+                if (Day >= 1 && Day <= 30)
+                    Console.WriteLine("{0}/{1}/{2} la ngay hop le", Day, Month, Year);
+                else
+                    Console.WriteLine("{0} ngay khong hop le", Day);
+            }
+
+            if (Month == 2)
+            {
+                if (Year % 400 == 0 || Year % 4 == 0 && Year % 100 != 0)
+                {
+                    if (Day >= 1 && Day <= 29)
+                        Console.WriteLine("{0}/{1}/{2} la ngay hop le", Day, Month, Year);
+                    else
+                        Console.WriteLine("{0} ngay khong hop le", Day);
+                }
+                else
+                {
+                    if (Day >= 1 && Day <= 28)
+                        Console.WriteLine("{0}/{1}/{2} la ngay hop le", Day, Month, Year);
+                    else
+                        Console.WriteLine("{0} ngay khong hop le", Day);
+                }
+            }
+
+        }
+    }
+}
---
+++ bai2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hw_2
+{
+    class bai2
+    {
+        static void Main(string[] args)
+        {
+            //menu cac phep toan
+            Console.WriteLine("\t**Calculator**");
+            Console.WriteLine("(+): phep cong");
+            Console.WriteLine("(-): phep tru");
+            Console.WriteLine("(*): phep nhan");
+            Console.WriteLine("(/): phep chia");
+            Console.WriteLine("[Start]");
+
+            int a, b;       //bien luu 2 so nguyen
+            String oprt, x;    //bien luu toan tu
+
+            do
+            {
+                Console.WriteLine("nhap 2 so va toan tu:");
+                Console.Write("a = ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("b = ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.Write("operator: ");
+                oprt = Console.ReadLine();
+                switch (oprt)
+                {
+                    case "+":
+                        {
+                            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+                            break;
+                        }
+                    case "-":
+                        {
+                            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+                            break;
+                        }
+                    case "*":
+                        {
+                            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+                            break;
+                        }
+                    case "/":
+                        {
+                            Console.WriteLine("{0} / {1} = {2} (modulo = {3})", a, b, a / b, a%b);
+                            break;
+                        }
+                }
+                Console.WriteLine("Continue??? y/n ([y]: yes/ [n]: no)");
+                x = Console.ReadLine();
+            }
+            while (x == "y");
+            Console.WriteLine("[End]");
+        }
+    }
+}
