e6340f83c52f29ce65e945601ab275c592dba500
VietTT23 <62922146+VietTT23@users.noreply.github.com>
2020-06-23 00:34:38 +0700

    Add files via upload

---
+++ bai4_class/CONNGUOI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bai4_class
+{
+    class CONNGUOI
+    {
+        protected string hoTen, queQuan, gioiTinh, ptDiLai;
+        protected int tuoi;
+        public CONNGUOI() { }
+        public CONNGUOI(string ht, string qq, string gt, string ptdl, int tu)
+        {
+            hoTen = ht;
+            queQuan = qq;
+            gioiTinh = gt;
+            ptDiLai = ptdl;
+            tuoi = tu;
+        }
+        public void input()
+        {
+            Console.Write("Ho Ten: ");
+            hoTen = Console.ReadLine();
+            Console.Write("Que Quan: ");
+            queQuan = Console.ReadLine();
+            Console.Write("Gioi Tinh: ");
+            gioiTinh = Console.ReadLine();
+            Console.Write("Phuong Tien Di Lai: ");
+            ptDiLai = Console.ReadLine();
+            Console.Write("Tuoi: ");
+            tuoi = Convert.ToInt32(Console.ReadLine());
+        }
+        public void output()
+        {
+            Console.WriteLine(hoTen);
+            Console.WriteLine(queQuan);
+            Console.WriteLine(gioiTinh);
+            Console.WriteLine(ptDiLai);
+            Console.WriteLine(tuoi);
+        }
+    }
+
+    class 
+}
---
+++ bai4_class/bai4_class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bai4_class
+{
+    class bai4_class
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
