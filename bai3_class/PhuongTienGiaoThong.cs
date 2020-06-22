6327d4c3a11bb0cc280cc31c1253e8ffe2c0eb31
VietTT23 <62922146+VietTT23@users.noreply.github.com>
2020-06-22 23:50:03 +0700

    Add files via upload

---
+++ bai3_class/PhuongTienGiaoThong.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hw3
+{
+    abstract class PhuongTienGiaoThong
+    {
+        public abstract void move();
+    }
+
+    class OTO : PhuongTienGiaoThong
+    {
+        public override void move()
+        {
+            Console.WriteLine("Oto di chuyen bang 4 banh");
+        }
+    }
+
+    class XEMAY : PhuongTienGiaoThong
+    {
+        public override void move()
+        {
+            Console.WriteLine("Xe may di chuyen bang 2 banh va su dung dong co");
+        }
+    }
+
+    class TAUTHUY : PhuongTienGiaoThong
+    {
+        public override void move()
+        {
+            Console.WriteLine("Tau thuy di chuyen bang chan vit vs su dung dong co");
+        }
+    }
+
+    class MAYBAY : PhuongTienGiaoThong
+    {
+        public override void move()
+        {
+            Console.WriteLine("May bay di chuyen bang canh quat va su dung dong co");
+        }
+    }
+}
---
+++ bai3_class/bai3_class.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hw3
+{
+    class bai3_class
+    {
+        static void Main(string[] args)
+        {
+            PhuongTienGiaoThong x;
+            string y;
+            Console.WriteLine("Ban muon di chuyen bang phuong tien nap?");
+            Console.WriteLine("1. OTO       (phím 1)");
+            Console.WriteLine("2. XEMAY     (phím 2)");
+            Console.WriteLine("3. TAUTHUY   (phím 3)");
+            Console.WriteLine("4. MAYBAY    (phím 4)");
+            Console.Write("[Enter] : ");
+            y = Console.ReadLine();
+            switch (y)
+            {
+                case "1":
+                    {
+                        x = new OTO();
+                        x.move();
+                        break;
+                    }
+                case "2":
+                    {
+                        x = new XEMAY();
+                        x.move();
+                        break;
+                    }
+                case "3":
+                    {
+                        x = new TAUTHUY();
+                        x.move();
+                        break;
+                    }
+                case "4":
+                    {
+                        x = new MAYBAY();
+                        x.move();
+                        break;
+                    }
+            }
+        }
+    }
+}
