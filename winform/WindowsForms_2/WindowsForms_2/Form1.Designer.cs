38c226545b7945855a3ad4751c193ecf264135bc
VietTT23 <62922146+VietTT23@users.noreply.github.com>
2020-06-29 18:16:38 +0700

    Add files via upload

--- winform/WindowsForms_2/WindowsForms_2/Form1.Designer.cs
+++ winform/WindowsForms_2/WindowsForms_2/Form1.Designer.cs
@@ -34,6 +34,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -81,19 +85,54 @@
             this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label4.Location = new System.Drawing.Point(15, 183);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(144, 13);
+            this.label4.Size = new System.Drawing.Size(150, 13);
             this.label4.TabIndex = 4;
-            this.label4.Text = "Các số hoàn chỉnh nỏ hơn n:";
+            this.label4.Text = "Các số hoàn chỉnh nhỏ hơn n:";
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(15, 227);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
+            this.button1.TabIndex = 2;
             this.button1.Text = "Hiển thị";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(239, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(282, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(239, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(282, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(239, 180);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(282, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(446, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -102,7 +141,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WindowsForms_2.Properties.Resources.Untitled;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(533, 273);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -124,6 +168,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- winform/WindowsForms_2/WindowsForms_2/Form1.cs
+++ winform/WindowsForms_2/WindowsForms_2/Form1.cs
@@ -18,36 +18,89 @@ namespace WindowsForms_2
         int n;
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            #region Kiểm tra dữ liệu nhập vào
+
             if (!int.TryParse(textBox1.Text, out n))
             {
                 MessageBox.Show("Nhập sai dữ liệu");
-                textBox1.Focus();
+                //textBox1.Focus();
             }
+
+            #endregion
         }
 
         private void textBox1_Leave(object sender, EventArgs e)
         {
-            if (n >= 0 && n <= 1000)
+            #region Kiểm tra điều kiện 0 <= n <= 1000
+
+            if (n < 0 || n > 1000)
             {
                 MessageBox.Show("Vui lòng nhập dữ liệu trong khoảng từ 0 đến 1000");
                 textBox1.Focus();
             }
+
+            #endregion
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //int count;
-            for (int i = 2; i <= Math.Sqrt(n); i++)
+            n = Convert.ToInt32(textBox1.Text);
+            #region Hiển thị các số nguyên tố < n
+
+            for (int i = 2; i < n; i++)
             {
+                int count = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    textBox2.Text += " " + i + ", ";
+                }
             }
+
+            #endregion
+
+            #region Hiển thị các số chính phương < n
 
             for (int i = 0; i < n; i++)
             {
                 if ((int)(Math.Sqrt(i)) == Math.Sqrt(i))
                 {
-                    label3.Text = Convert.ToString(i);
+                    textBox3.Text += " " + i + ", ";
                 }
             }
+
+            #endregion
+
+            #region Hiển thị các số hoàn chỉnh < n
+            
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                if (sum == i)
+                {
+                    textBox4.Text += " " + i + ", ";
+                }
+            }
+
+            #endregion
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
